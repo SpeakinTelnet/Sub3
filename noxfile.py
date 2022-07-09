491f9771a00c6f824417f1819deac53e95838c46
SpeakinTelnet <gui.lac@protonmail.com>
2022-07-09 11:30:00 -0400

    Added a release session

--- noxfile.py
+++ noxfile.py
@@ -34,3 +34,46 @@ def docs(session):
 
     # Generate documentation into `build/docs`
     session.run("sphinx-build", "-W", "-b", "html", "-v", "docs/", "docs/_build/html")
+
+@nox.session
+def release(session: nox.Session) -> None:
+
+    wheel(session)
+
+    """
+    Kicks off an automated release process by creating and pushing a new tag.
+
+    Invokes bump2version with the posarg setting the version.
+
+    Usage:
+    $ nox -s release -- [major|minor|patch]
+    """
+    parser = argparse.ArgumentParser(description="Release a semver version.")
+    parser.add_argument(
+        "version",
+        type=str,
+        nargs=1,
+        help="The type of semver release to make.",
+        choices={"major", "minor", "patch"},
+    )
+    args: argparse.Namespace = parser.parse_args(args=session.posargs)
+    version: str = args.version.pop()
+
+    # If we get here, we should be good to go
+    # Let's do a final check for safety
+    confirm = input(
+        f"You are about to bump the {version!r} version. Are you sure? [y/n]: "
+    )
+
+    # Abort on anything other than 'y'
+    if confirm.lower().strip() != "y":
+        session.error(f"You said no when prompted to bump the {version!r} version.")
+
+    session.install("bump2version")
+
+    session.log(f"Bumping the {version!r} version")
+    session.run("bump2version", version)
+
+    session.log("Pushing the new tag")
+    session.run("git", "push", external=True)
+    session.run("git", "push", "--tags", external=True)
